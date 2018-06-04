e54e7bedf601364a59ab08a1011786b7a7b8a577
theayayron <aaronkawahara@gmail.com>
2018-06-04 13:42:32 -0700

    template for float_add complete

--- lab6.c
+++ lab6.c
@@ -5,33 +5,24 @@
 
 /* Example test case 3.5 */
 
-void extract_float_test(float x) {
-  INTFLOAT y;
-  extract_float(x, &y);
-  printf("Float is: %3.1f Fraction is: 0x%08X, Exponent is: 0x%08X\n", x, y.fraction, y.exponent);
+void add_float_test(float x) {
+    INTFLOAT y;
+    add_float(x, &y);
+  
 }
 
-int main()
-  {
+int main() {
     INTFLOAT x;
-  unsigned int i;
+    unsigned int i;
 
-  extract_float_test( 4.0);
-  extract_float_test(1.0);
-  extract_float_test(-4.0);
-  extract_float_test(62.5);
-  extract_float_test(-33.0);
+    printf("Now do a performance test, repeating 1x10^9 times.\n");
 
-  printf("Now do a performance test, repeating 1x10^9 times.\n");
+    /* repeat count = 1000000000 = 1 x 10^9 = 1 billion times */
 
-/* repeat count = 1000000000 = 1 x 10^9 = 1 billion times */
-
-  for (i=0; i<1000000000; i++)
-     {
-     extract_float( 3.3, &x);
-     }
-  printf("Ending performance test.\n");
-
-
-  }
+    for (i=0; i<1000000000; i++)
+	{
+	    /* add_float(); */
+	}
+    printf("Ending performance test.\n");
+}
 
--- lab6a.c
+++ lab6a.c
@@ -4,34 +4,82 @@
 
 /* Fully-implemented extract_float */
 
+float float_add(float a, float b) {
+    INTFLOAT x, y, result;
+    float sum = 0;
+    unsigned int *sum_ptr = &sum;
+    
+    /* extract two's compliment components of the two given floats */
+    extract_float(a, &x);
+    extract_float(b, &y);
+
+    if(x.exponent > y.exponent) {
+	/* shift y fraction right and increase expononent until equal */
+    } else if(x.exponent < y.exponent) {
+	/* same as above but for x */
+    } 
+    
+    if(x.exponent == y.exponent) {
+	result.exponent = x.exponent + 1;
+    } else {
+	printf("Error: exponents not equal\n");
+    }
+
+    a.fraction = a.fraction >> 1;
+    b.fraction = b.fraction >> 1;
+    result.fraction = a.fraction + b.fraction;
+
+    /* normalize result left shifting and decrementing exponent */
+
+    if(result.fraction == 0) {
+	return 0;
+    } else if (result.fraction < 0) {
+	float_result |= 0x80000000;
+	if(result.fraction == 0x80000000) {
+	    result.fraction = result.fraction >> 1;
+	    result.exponent++;
+	    result.fraction = - result.fraction;
+	}
+    }
+
+    /* copy fraction field from bits after hidden 1 */
+    result.exponent += 126;
+    /* move fields into correct positions */
+
+    return 0;
+}
+
+float hardware_float_add(float a, float b) {
+    return a + b;
+}
 
 void extract_float(float f, INTFLOAT_PTR xp)
-  {
-  unsigned  int uintf;
+{
+    unsigned  int uintf;
 
-  uintf = (unsigned int) *(unsigned int *) &f;  /* Get unsigned int version of float */
+    uintf = (unsigned int) *(unsigned int *) &f;  /* Get unsigned int version of float */
 
-  if (uintf == 0)
-     {
-     xp->fraction = 0;
-     xp->exponent = 0;
-     return;
-     }
+    if (uintf == 0)
+	{
+	    xp->fraction = 0;
+	    xp->exponent = 0;
+	    return;
+	}
 
- /* extracting exponent */
-  xp->exponent = ((int)((uintf & 0x7FFFFFFF) >> 23)) - 126;
+    /* extracting exponent */
+    xp->exponent = ((int)((uintf & 0x7FFFFFFF) >> 23)) - 126;
 
-  /* extracting fraction */
-  xp->fraction = 0x00800000;
-  xp->fraction = xp->fraction | uintf;
-  xp->fraction = xp->fraction & 0x00FFFFFF;
-  xp->fraction = xp->fraction << 7;
+    /* extracting fraction */
+    xp->fraction = 0x00800000;
+    xp->fraction = xp->fraction | uintf;
+    xp->fraction = xp->fraction & 0x00FFFFFF;
+    xp->fraction = xp->fraction << 7;
 
-  /* testing signed bit */
-  if(f < 0) {
-    xp->fraction = -xp->fraction;
-  }
+    /* testing signed bit */
+    if(f < 0) {
+	xp->fraction = -xp->fraction;
+    }
 
-  return;
-  }
+    return;
+}
 
